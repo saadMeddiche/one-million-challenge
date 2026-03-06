7de34c83aac1e0dc9c68ee14ee929ca0f05bf077
SaadMeddiche <saadmeddiche2004201@gmail.com>
2026-03-06 10:37:51 +0000

    feat(ChunkTxtFileGenerator): implement a new process that generate/write content in chunks.

--- src/main/java/org/saadMeddiche/processes/impl/ChunkTxtFileGenerator.java
+++ src/main/java/org/saadMeddiche/processes/impl/ChunkTxtFileGenerator.java
@@ -2,9 +2,15 @@ package org.saadMeddiche.processes.impl;
 
 import org.saadMeddiche.models.TxtFileGeneratorResult;
 import org.saadMeddiche.processes.TxtFileGenerator;
+import org.saadMeddiche.utils.FasterRandom;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -16,8 +22,60 @@ public class ChunkTxtFileGenerator extends TxtFileGenerator {
 
     private final static Logger logger = Logger.getLogger(ChunkTxtFileGenerator.class.getName());
 
+    private final static int CHUNK_SIZE = 1_000_000;
+
     protected TxtFileGeneratorResult mainProcess(File file, long lines) {
+
+        List<String> chunk = new ArrayList<>(CHUNK_SIZE);
+
+        for (int i = 0; i < lines; i++) {
+
+            chunk.add(buildLine(i));
+
+            if( chunk.size() == CHUNK_SIZE ) {
+                boolean isContentWritten = writeIntoFile(file, chunk);
+                if( !isContentWritten ) {
+                    logger.log(Level.WARNING, "Could not write chunk at line " + i + ".");
+                } else {
+                    logger.log(Level.INFO, "Successfully generated chunk at line " + i + ".");
+                }
+                chunk.clear();
+            }
+
+        }
+
+        if( !chunk.isEmpty() ) {
+            writeIntoFile(file,chunk);
+            chunk.clear();
+        }
+
         return new TxtFileGeneratorResult(true, Optional.empty());
+    }
+
+    private String buildLine(long id) {
+        return id + COLUMN_SEPARATOR + FasterRandom.uuid() + COLUMN_SEPARATOR + FasterRandom.number();
+    }
+
+    private boolean writeIntoFile(File file, List<String> chunk) {
+
+        try(FileWriter fileWriter = new FileWriter(file, true)) {
+
+            String content = buildContent(chunk);
+
+            fileWriter.write(content);
+
+            return true;
+
+        }
+        catch (IOException e) {
+            logger.warning("Failed to write to file due tue " + e.getMessage());
+            return false;
+        }
+
+    }
+
+    private String buildContent(List<String> chunk) {
+        return String.join(System.lineSeparator(), chunk);
     }
 
 }
