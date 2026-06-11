dc6a0fd70d862f35f2bc28758083be12ac133cc0
SaadMeddiche <saadmeddiche2004201@gmail.com>
2026-06-11 11:14:05 +0100

    correct SOUTs and update constants

--- src/main/java/org/saadMeddiche/App.java
+++ src/main/java/org/saadMeddiche/App.java
@@ -2,6 +2,7 @@ package org.saadMeddiche;
 
 import org.saadMeddiche.processes.extractors.TxtFileExtractor;
 import org.saadMeddiche.processes.extractors.impl.*;
+import org.saadMeddiche.processes.extractors.impl.emptyVersions.*;
 
 import org.saadMeddiche.processes.generators.TxtFileGenerator;
 import org.saadMeddiche.processes.generators.impl.*;
@@ -17,13 +18,13 @@ public class App
 {
 
     public static final long LINES_TO_GENERATE = 100_000_000L;
-    public static final String FILE_NAME_TO_READ_FROM = "one-million-challenge";
+    public static final String FILE_NAME_TO_READ_FROM = "one-million-challenge_sample";
     public static final String FILE_NAME_TO_WRITE_INTO = "one-million-challenge";
 
-    private static final int SCRIPT_MOD = 0; // == 0 -> generation ; != 0 -> extraction
+    private static final int SCRIPT_MOD = 1; // == 0 -> generation ; != 0 -> extraction
 
     private static final TxtFileGenerator GENERATOR = new SeekableByteChannelTxtFileGenerator();
-    private static final TxtFileExtractor EXTRACTOR = new SeekableByteChannelTxtFileExtractor();
+    private static final TxtFileExtractor EXTRACTOR = new EmptyStreamTxtFileExtractor();
 
     public static void main( String[] args )
     {
@@ -60,6 +61,7 @@ public class App
         }
         else {
             System.out.printf("├SCRIPT: generation_script #failed #reason(%s)", generationResult.failReason());
+            System.out.println();
         }
 
         System.out.println("│");
@@ -79,12 +81,13 @@ public class App
         var extractionResult = response.result();
 
         if(extractionResult.success()) {
-            System.out.printf("├SCRIPT: extraction_script #success #time(%s ms) #sum(%s$%b)",
-                    response.time(), extractionResult.totalSum(), -15673375262273L == extractionResult.totalSum());
+            System.out.printf("├SCRIPT: extraction_script #success #sum(%s$%b)",
+                    extractionResult.totalSum(), -15673375262273L == extractionResult.totalSum());
         } else {
             System.out.printf("├SCRIPT: extraction_script #failed #reason(%s)", extractionResult.failReason());
         }
 
+        System.out.println();
         System.out.println("│");
         System.out.printf("└SCRIPT: extraction_script #ended #time(%s ms)", response.time());
         System.out.println();
